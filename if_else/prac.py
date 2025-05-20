06ea5e2d664fb65bd15e29fd1382c94a9c8ab208
tejas200411 <sunchutejas@gmail.com>
2025-05-20 19:53:54 +0530

    learnt if and if else

---
+++ if_else/prac.py
@@ -0,0 +1,14 @@
+# vishwa= 9959
+
+# if vishwa == 99599:
+#     print("user is correct")
+
+# print("this is running")
+
+
+phno= 9290758
+
+if phno > 1000:
+    print("correct")
+else:
+    print("wrong")
