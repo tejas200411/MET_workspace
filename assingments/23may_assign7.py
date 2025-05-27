ab2173c1b2085b17170edcde204a4b514b37c63c
tejas200411 <sunchutejas@gmail.com>
2025-05-27 23:56:44 +0530

    done

--- assingments/23may_assign7.py
+++ assingments/23may_assign7.py
@@ -1,5 +1,5 @@
 # Input a sentence in all caps. Convert only the first word to title case, and the rest to lowercase.
 # Hint: Use .capitalize() and .lower().
 
-v=input("enter a sentence")
+v=input("enter a sentence: ")
 print(v.capitalize())
